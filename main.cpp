a127dec4b5929e7fc7d6d172cc70a1d651ced325
Gustavo Mendes <gustavo.pm2003@gmail.com>
2020-05-19 10:28:34 -0300

    Cabecalho

--- main.cpp
+++ main.cpp
@@ -4,6 +4,55 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int QuanCompra, PagamentoAtrasado, ScoreV, ScoreI, ScoreFP, ScoreF;
+    double ValordaCompra;
+    char TipodaCompra;
+
+    cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
+
+    cout << "--------------------------------" <<endl;
+
+    cout << "INFORME OS DADOS DO ULTIMO ANO" <<endl;
+
+    cout << endl;
+
+    cout << "Quantas compras o cliente fez no ultimo ano? ";
+
+    cin >> QuanCompra;
+
+    cout << "Qual o ticket medio? ";
+
+    cin >> ValordaCompra;
+
+    cout << endl;
+
+    cout << "Quantas vezes o cliente atrasou o pagamento? ";
+
+    cin >> PagamentoAtrasado;
+
+    cout << "A maioria das compras foi em dinheiro, cartao, ou boleto (D/C/B)? ";
+
+    cin >> TipodaCompra;
+
+    cout << endl;
+
+    cout << "Score de volume de compras = " << ScoreV << endl ;
+
+    cout << endl;
+
+    cout << "Score de inadimplencia = "<< ScoreI << endl;
+
+    cout <<endl;
+
+    cout << "Score de forma de pagamento = "<< ScoreFP <<endl;
+
+    cout << endl;
+
+    cout << "Classificação final = " << "CLIENTE BRONZE" << endl;
+
+    cout << "Classificação final = " << "CLIENTE PRATA"<< endl;
+
+    cout << "Classificação final = " << "CLIENTE OURO"<< endl;
+
     return 0;
 }
